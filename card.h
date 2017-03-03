4958170c001f0aeb783337bb82ad78d7d0a3d595
Georg Wolffgang <Shazzarr@Georgs-MBP.fritz.box>
2017-03-03 15:45:08 +0100

    card.h owner/getter/setter added

--- card.h
+++ card.h
@@ -10,24 +10,34 @@
 class Card : public QObject, public QGraphicsPixmapItem {
 
 private:
+    // variables
+    QString name = "";
+    int choice[4][2] = {};
+    int owner = 0;          // 0 = neutral, 1 = player white, 2 = player black
 
 public:
     // variables
-    QString name;
     int id;
-    int choice[4][2] = {};
 
     // constructors
     Card(QGraphicsItem *parent = NULL);
 
-    // functions
+    // getters
+    inline QString getName() { return name; }
+    inline int getOwner() { return owner; }
+    inline int getXFromChoice(const int number) { return choice[number][0]; }
+    inline int getYFromChoice(const int number) { return choice[number][1]; }
+
+    // setters
+    inline void setName(int newName) { name = newName; }
+    inline void setOwner(int newOwner) { owner = newOwner; }
+
+    //functions
     void setCardValues(int cardID, double slotWidth, double slotHeight, double border_x, double border_y);
-    inline int get_x_from_choice(const int number) { return choice[number][0]; }
-    inline int get_y_from_choice(const int number) { return choice[number][1]; }
 
     // operators
-    inline bool operator==(const Card &compare) { return (name == compare.name); }
-    inline bool operator!=(const Card &compare) { return (name != compare.name); }
+    inline bool operator==(const Card *compare) { return (name == compare->name); }
+    inline bool operator!=(const Card *compare) { return (name != compare->name); }
 };
 
 #endif // CARD_H
