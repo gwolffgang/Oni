64041846dcc0272c38de2ac11a1806c684d363f0
alexandartomic <alex.tomic92@gmail.com>
2017-03-23 13:02:42 +0100

    resizing aboutwindow

--- aboutwindow.cpp
+++ aboutwindow.cpp
@@ -1,12 +1,9 @@
 #include <QApplication>
 #include "aboutwindow.h"
 #include "ui_aboutwindow.h"
-#include <QTextBrowser>
 
 AboutWindow::AboutWindow(QWidget *parent) : QDialog(parent), ui(new Ui::AboutWindow) {
     ui->setupUi(this);
-    ui->textLicenses->setOpenExternalLinks(true);
-    ui->textAbout->setOpenExternalLinks(true);
 }
 
 AboutWindow::~AboutWindow() {
